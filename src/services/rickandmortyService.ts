3cdc116de2f9836dbef3bbcbca3ad8162b0a7e79
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-02-21 10:53:26 -0300

    requisições

---
+++ src/services/rickandmortyService.ts
@@ -0,0 +1,17 @@
+const BASE_URL = "https://rickandmortyapi.com/api";
+
+export const fetchCharacters = async () => {
+  const response = await fetch(`${BASE_URL}/character`);
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchCharacterDetail = async (id: number) => {
+  const response = await fetch(`${BASE_URL}/character/${id}`);
+
+  const data = await response.json();
+
+  return data;
+};
